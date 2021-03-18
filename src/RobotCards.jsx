13970662e82a74720c648546bd6c9f2ef86792ad
Arpita Saha <arpita.sahaa@gmail.com>
2021-03-17 20:26:42 -0400

    added gh packages

--- src/RobotCards.jsx
+++ src/RobotCards.jsx
@@ -4,9 +4,9 @@ import "./RobotCards.css";
 function RobotCards(props) {
   return props.robotsF.map((rbt, i) => {
     return (
-      <div className="card cardRobot">
+      <div className="card cardRobot" key={i}>
         <div className="card-body">
-          key={i}
+          
           <img src={`https://robohash.org/${rbt.id}`} alt="robot"></img>
           <h5 className="card-title">{rbt.name}</h5>
           <h6>{rbt.email}</h6>
